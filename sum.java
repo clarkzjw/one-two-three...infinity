f4ad6f361e7e57a6812e2ae46c3fb3226a0fee25
Jinwei Zhao <clarkzjw@gmail.com>
2014-12-01 20:28:09 +0800

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+public static void main(String[] args) {
+          long n = 0;
+          for (int i = 0; i < 1000000; i++) {
+               n=n+i;
+          }
+          System.out.println(n);
+     }
+}
