da3b594ec9368916f1c02237a9d4866b118c780b
JinweiClarkChao <clarkzjw@gmail.com>
2014-12-01 21:19:33 +0800

    fix java start value

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum {
 public static void main(String[] args) {
           long n = 0;
-          for (int i = 0; i < 1000000000; i++) {
+          for (int i = 1; i < 1000000000; i++) {
                n=n+i;
           }
           System.out.println(n);
