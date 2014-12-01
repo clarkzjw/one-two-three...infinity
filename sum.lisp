0a7254b482bc848c9935037fa9374bb48d4cdb2b
chaosink <475936052@qq.com>
2014-12-01 20:51:01 +0800

    1000 to 1000000

--- sum.lisp
+++ sum.lisp
@@ -1,4 +1,4 @@
-(setf sum 0)
-(loop for i from 1 to 1000 do
-      (incf sum i))
-(format t "~d" sum)
+(defvar *sum* 0)
+(loop for i from 1 to 1000000 do
+      (incf *sum* i))
+(format t "~d" *sum*)
