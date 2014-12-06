f8f9122fc5da7b6cfee9c1f0f1c99c95279d59fe
Jinwei Zhao <clarkzjw@gmail.com>
2014-12-06 17:59:08 +0800

    java BigInteger

--- sum.java
+++ sum.java
@@ -1,9 +1,15 @@
-public class sum {
-public static void main(String[] args) {
-          long n = 0;
-          for (int i = 1; i < 1000000000; i++) {
-               n=n+i;
-          }
-          System.out.println(n);
-     }
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class sum
+{
+	public static void main(String args[])
+	{
+		int n = 1000000000;
+		BigInteger ans = BigInteger.ZERO;
+		for(int i = 1; i <= n; i++)
+			ans = ans.add(BigInteger.valueOf(i));
+		System.out.println(ans);
+	}
 }
