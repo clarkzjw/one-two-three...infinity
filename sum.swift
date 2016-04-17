f1bab8e793c9caf2a0cb17459e6026519a4d60bc
Anton Lindroth <Anton2001lindroth@gmail.com>
2016-04-17 22:37:05 +0200

    Update sum.swift

--- sum.swift
+++ sum.swift
@@ -1,6 +1,6 @@
 class ViewController: UIViewController {
     func viewDidLoad() {
         let n = 1000000000
-   	print (Float(n) * Float(n) / Float(2) + Float(n) / Float(2))
+   	    print (Float(n) * Float(n) / Float(2) + Float(n) / Float(2))
     }
 }
