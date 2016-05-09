b8db4ab97f7542b2f734984dbc0ff2f4c0b4874d
Jinwei Zhao <i@ring0.xyz>
2016-05-09 14:11:38 +0800

    use openmp for c++

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <omp.h>
+
+using namespace std;
+
 int main()
 {
 	long long sum = 0;
+
+	#pragma omp for reduction(+:sum)
 	for (int i = 1; i <= 1000000000; i++)
 		sum += i;
-	std::cout<<sum<<std::endl;
+	cout << sum << endl;
 	return 0;
 }
