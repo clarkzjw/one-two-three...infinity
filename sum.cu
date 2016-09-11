d6fe32d3a15e8c124fcc88c5b6b5f1d3ec4ea5e1
Troels Henriksen <athas@sigkill.dk>
2016-09-11 09:40:16 +0200

    Nicer Thrust implementation. (#32)

--- sum.cu
+++ sum.cu
@@ -1,18 +1,15 @@
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/generate.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/reduce.h>
 #include <iostream>
 
 int main(void)
 {
-	using namespace std;
-	thrust::host_vector<int> h_vec(1000000000);
-	
-	int i = 1;
-	for (auto iter = h_vec.begin(); iter != h_vec.end(); iter++, i++)
-		*iter = i;
-	thrust::device_vector<int> d_vec = h_vec;
-        long long sum = thrust::reduce(d_vec.begin(), d_vec.end(), 0, thrust::plus<int>());
-	cout << sum << endl;
-	return 0;
+  thrust::counting_iterator<int64_t> start(1);
+  int64_t sum = thrust::reduce(start,
+                               start + 1000000000,
+                               0,
+                               thrust::plus<int64_t>());
+
+  std::cout << sum << std::endl;
+  return 0;
 }
